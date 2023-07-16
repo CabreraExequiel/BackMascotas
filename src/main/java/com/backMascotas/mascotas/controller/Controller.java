8fb58ef237ef86be90290b7a0f9ac2340902ff9f
exequiel <conan34681335@gmail.com>
2023-07-16 06:47:42 -0300

    prueba render

--- src/main/java/com/backMascotas/mascotas/controller/Controller.java
+++ src/main/java/com/backMascotas/mascotas/controller/Controller.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+@CrossOrigin(origins = "https://appmascotas10.web.app", allowedHeaders = "*")
 public class Controller {
     
     @Autowired
